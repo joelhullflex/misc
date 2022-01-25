6023c3df96ee535ab0ed396354e9fdd17ccd7a04
Joel Hull <joel.hull@getflex.com>
2022-01-25 16:00:55 -0600

    update

--- hc/config/hcserver.py
+++ hc/config/hcserver.py
@@ -2,14 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hcserver')
 def hello():
-    return '<h1>Hello, World!</h1>'
+    return '<h1>HC Server Hello, World!</h1>'
 
 @app.route('/status/healthcheck')
 def health_check():
     return "<h1>Healthy</h1>"
 
-@app.route('/testreq')
+@app.route('/hcserver/testreq')
 def testreq():
-    return "<h1>Test Request</h1>"
+    return "<h1>HC Server Test Request</h1>"
